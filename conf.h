18f3d04783d967e8dee5ea534bb753d87682f737
Lich <lich0x2b@gmail.com>
2009-10-14 15:07:22 +0300

    first commit

--- conf.h
+++ conf.h
@@ -15,16 +15,16 @@
 #define KEY_PREV	'p' /* select previous window in list */
 #define KEY_NEXT	'n' /* select next window in list */
 #define KEY_STATUS	's' /* show message containing output from the command defined at STATUS */
-#define TERMINAL	"urxvt -display :0" /* terminal */
+#define TERMINAL	"urxvt" /* terminal */
 #define MENU		"`dmenu_path | dmenu -fn '-xos4-terminus-*-r-*-*-12-*-*-*-*-*-*-*' -nb '#222222' -nf '#FFFFFF' -sf '#ffffff' -sb '#666666'`"
 /* launcher menu to run */
 #define FONT		"-xos4-terminus-*-*-*-*-12-*-*-*-*-*-*-*"
 /* font to use */
-#define BORDER		1 /* border size for window lists / input box */
-#define FGCOLOR		"#909090" /* window list and messaging foreground color */
-#define BGCOLOR		"#191919" /* window list and messaging background color */
-#define SELFGCOLOR	"#000000" /* window list foreground for selected window */
-#define SELBGCOLOR	"#909090" /* window list background for selected window */
+#define BORDER		2 /* border size for window lists / input box */
+#define FGCOLOR		"#cfdb6c" /* window list and messaging foreground color */
+#define BGCOLOR		"#222222" /* window list and messaging background color */
+#define SELFGCOLOR	"#222222" /* window list foreground for selected window */
+#define SELBGCOLOR	"#edf69f" /* window list background for selected window */
 #define PADDING_NORTH	0 /* top screen edge unmanaged pixels */
 #define PADDING_WEST	0 /* left screen edge unmanaged pixels */
 #define PADDING_SOUTH	0 /* bottom screen edge unmanaged pixels */
